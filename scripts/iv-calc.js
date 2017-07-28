3ff695efdec8a42c9c2b8dc28ef22bd4cff59837
Razator73 <ryan.t.scott73@gmail.com>
2017-07-28 15:00:13 -0600

    Update iv-calc.js

--- scripts/iv-calc.js
+++ scripts/iv-calc.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
   var pokemon = $('#pokeList');
   pokemon.empty();
+  pokemon.append('<option value="Testing" />');
   
   var natures = $('#natList');
   natures.empty();
